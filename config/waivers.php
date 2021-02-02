db73e56e1a80c7be5083dfbe7e172cd678ce95fa
Chris Hackett <crishack8@gmail.com>
2021-02-02 16:54:00 -0600

    Add model class references.

--- config/waivers.php
+++ config/waivers.php
@@ -2,4 +2,19 @@
 
 return [
 
+    /**
+     * Model class references
+     */
+    'model_class' => [
+        'room' => \App\Models\Room::class,
+
+        'booking' => \App\Models\Booking::class,
+
+        'location' => \App\Models\Location::class,
+
+        'image' => \DrewRoberts\Media\Models\Image::class,
+
+        'participant' => \App\Models\Participant::class,
+    ]
+
 ];

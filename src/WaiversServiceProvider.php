60d2f64af4d4d3dd878f6f90de4011a67f102fba
Phuc Le <phuclh93@gmail.com>
2021-02-09 15:46:46 -0800

    Load migrations from package.

--- src/WaiversServiceProvider.php
+++ src/WaiversServiceProvider.php
@@ -2,13 +2,18 @@
 
 namespace Tipoff\Waivers;
 
-use Illuminate\Support\Str;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Tipoff\Waivers\Commands\WaiversCommand;
 
 class WaiversServiceProvider extends PackageServiceProvider
 {
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        parent::boot();
+    }
+
     public function configurePackage(Package $package): void
     {
         /*
@@ -19,23 +24,6 @@ class WaiversServiceProvider extends PackageServiceProvider
         $package
             ->name('waivers')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('2020_05_11_110000_create_signatures_table')
-            ->hasCommand(WaiversCommand::class);
-    }
-
-    /**
-     * Using packageBooted lifecycle hooks to override the migration file name.
-     * We want to keep the old filename for now.
-     */
-    public function packageBooted()
-    {
-        foreach ($this->package->migrationFileNames as $migrationFileName) {
-            if (! $this->migrationFileExists($migrationFileName)) {
-                $this->publishes([
-                    $this->package->basePath("/../database/migrations/{$migrationFileName}.php.stub") => database_path('migrations/' . Str::finish($migrationFileName, '.php')),
-                ], "{$this->package->name}-migrations");
-            }
-        }
+            ->hasViews();
     }
 }

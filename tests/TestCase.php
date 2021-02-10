c16e5eb2f894e4cbe85f4f2148fcdb2d73dcadf2
Chris Hackett <crishack8@gmail.com>
2021-02-10 12:05:02 -0600

    Use BaseTestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,41 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tipoff\Waivers\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
+use Tipoff\TestSupport\BaseTestCase;
+use Tipoff\Support\SupportServiceProvider;
 use Tipoff\Waivers\WaiversServiceProvider;
 
-class TestCase extends Orchestra
+class TestCase extends BaseTestCase
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Tipoff\\Waivers\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
-            WaiversServiceProvider::class,
+            SupportServiceProvider::class,
+            WaiversServiceProvider::class
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('database.default', 'sqlite');
-        $app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
-
-        /*
-        include_once __DIR__.'/../database/migrations/create_waivers_table.php.stub';
-        (new \CreatePackageTable())->up();
-        */
     }
 }

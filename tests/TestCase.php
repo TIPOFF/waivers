5820ed48a84c0daedbcea22823b59ce6a057acba
Chris Hackett <crishack8@gmail.com>
2021-02-13 18:57:18 -0600

    Nova Signature Resource

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,17 +4,21 @@ declare(strict_types=1);
 
 namespace Tipoff\Waivers\Tests;
 
+use Laravel\Nova\NovaCoreServiceProvider;
 use Spatie\Permission\PermissionServiceProvider;
 use Tipoff\Authorization\AuthorizationServiceProvider;
 use Tipoff\Support\SupportServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
 use Tipoff\Waivers\WaiversServiceProvider;
+use Tipoff\Waivers\Tests\Support\Providers\NovaPackageServiceProvider;
 
 class TestCase extends BaseTestCase
 {
     protected function getPackageProviders($app)
     {
         return [
+            NovaCoreServiceProvider::class,
+            NovaPackageServiceProvider::class,
             SupportServiceProvider::class,
             AuthorizationServiceProvider::class,
             PermissionServiceProvider::class,

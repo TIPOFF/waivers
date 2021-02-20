f5350f024fd77e1b19191defd3a9fcc1d805acc3
Drew Roberts <github@drewroberts.com>
2021-02-20 12:05:26 -0500

    Rename the field for minor statement for clarity

--- database/migrations/2020_05_11_100000_create_waivers_table.php
+++ database/migrations/2020_05_11_100000_create_waivers_table.php
@@ -15,7 +15,7 @@ class CreateWaiversTable extends Migration
             $table->id();
             $table->foreignIdFor(Location::class)->unique();	// NOTE - unique() added -- there should be exactly one record per location!
             $table->text('waiver')->nullable(); // Waiver agreement for the location
-            $table->text('waiver_minor')->nullable(); // Waiver statement for parent/legal gaurdian of minors at the location
+            $table->text('minor_statement')->nullable(); // Waiver statement for parent/legal gaurdian of minors at the location
             $table->foreignIdFor(app('user'), 'creator_id');
             $table->foreignIdFor(app('user'), 'updater_id');
             $table->timestamps();

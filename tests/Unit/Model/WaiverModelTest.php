8ee8f5657456532a15452bcbe28305c606d4f6ef
Drew Roberts <github@drewroberts.com>
2021-02-20 12:02:41 -0500

    Update waiver test

--- tests/Unit/Model/WaiverModelTest.php
+++ tests/Unit/Model/WaiverModelTest.php
@@ -5,17 +5,17 @@ declare(strict_types=1);
 namespace Tipoff\Waivers\Tests\Unit\Models;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Tipoff\Waivers\Models\LocationWaiver;
+use Tipoff\Waivers\Models\Waiver;
 use Tipoff\Waivers\Tests\TestCase;
 
-class LocationWaiverModelTest extends TestCase
+class WaiverModelTest extends TestCase
 {
     use DatabaseTransactions;
 
     /** @test */
     public function create()
     {
-        $model = LocationWaiver::factory()->create();
+        $model = Waiver::factory()->create();
         $this->assertNotNull($model);
     }
 }

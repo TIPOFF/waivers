1924dcd4a7d3683c7beb824869cc65485b153831
wolfrednicolas <wolfrednicolas@users.noreply.github.com>
2021-02-26 23:53:59 +0000

    Fix styling

--- src/Http/Controllers/Web/WaiverController.php
+++ src/Http/Controllers/Web/WaiverController.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Tipoff\Waivers\Http\Controllers\Web;
 
-use Tipoff\Waivers\Events\WaiverSigned;
-use Illuminate\Routing\Controller;
-use Tipoff\Locations\Models\Location;
-use Tipoff\EscapeRoom\Models\Room;
-use Tipoff\Waivers\Models\Signature;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Tipoff\EscapeRoom\Models\Room;
+use Tipoff\Locations\Models\Location;
+use Tipoff\Waivers\Events\WaiverSigned;
+use Tipoff\Waivers\Models\Signature;
 
 class WaiverController extends Controller
 {
